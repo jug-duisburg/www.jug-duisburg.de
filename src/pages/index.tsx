bded40483c6b2ea72fbf0c7aafa27f47521920c9
Christian Kaltepoth <christian@kaltepoth.de>
2023-04-08 11:53:15 +0200

    Added logo to front page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
+import Image from 'next/image'
+import logo from '../images/logo.png'
+
 export default function Home() {
   return (
-    <main className="text-center mt-20">
-      <div>
+    <main className="flex flex-col items-center space-y-10 text-center px-10">
+
+      <Image src={logo} alt={"Logo"} width={500}/>
+
+      <div className="text-5xl">
         Homepage der JUG Duisburg
       </div>
+
+      <div className={"text-2xl"}>
+        Work in progress...
+      </div>
+
     </main>
   )
 }
